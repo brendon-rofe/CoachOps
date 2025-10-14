5fdbcf5395a8f030201e74bfda8e2892d669790e
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-10-14 09:31:58 +0200

    feat: Create initial Metric.tsx

---
+++ app/src/components/Metric.tsx
@@ -0,0 +1,26 @@
+import ProgressBar from "./ProgressBar";
+
+interface MetricProps {
+  title: string;
+  percent: number;
+  caption: string;
+}
+
+export default function Metric({ title, percent, caption }: MetricProps) {
+  return (
+    <section className="flex flex-col gap-3 p-4">
+      <div className="flex gap-6 justify-between">
+        <p className="text-white text-base font-medium leading-normal">
+          {title}
+        </p>
+        <p className="text-white text-sm font-normal leading-normal">
+          {percent}%
+        </p>
+      </div>
+      <ProgressBar percent={percent} />
+      <p className="text-[#92adc9] text-sm font-normal leading-normal">
+        {caption}
+      </p>
+    </section>
+  );
+}
