4126f5fd311e59228d85b6c3b6cf0808f3de10b4
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-10-16 14:03:05 +0200

    feat: Update dispaly to render progress bar based on data

--- app/src/pages/dashboard.tsx
+++ app/src/pages/dashboard.tsx
@@ -1,6 +1,13 @@
 import Metric from "../components/Metric";
+import { requestsData } from "../data";
+
+function getLatestStats() {
+  return requestsData[requestsData.length - 1];
+}
 
 export default function Dashboard() {
+  const latest = getLatestStats();
+
   return (
     <div>
       <div className="flex flex-wrap justify-between gap-3 p-4">
@@ -12,17 +19,29 @@ export default function Dashboard() {
       <h2 className="text-black dark:text-white text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 pb-3 pt-5">
         Connect Requests
       </h2>
-      <Metric title="Connect Requests Sent" percent={80} caption="120/150" />
+      <Metric
+        title="Connect Requests Sent"
+        percent={Math.round((latest.connect_requests_sent / 150) * 100)}
+        caption={`${latest.connect_requests_sent}/150`}
+      />
 
       <h2 className="text-black dark:text-white text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 pb-3 pt-5">
         Direct Messages
       </h2>
-      <Metric title="DMs Sent" percent={75} caption="250/333" />
+      <Metric
+        title="DMs Sent"
+        percent={Math.round((latest.dms_sent / 50) * 100)}
+        caption={`${latest.dms_sent}/50`}
+      />
 
       <h2 className="text-black dark:text-white text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 pb-3 pt-5">
         Calls
       </h2>
-      <Metric title="Calls Booked" percent={90} caption="50/55" />
+      <Metric
+        title="Calls Booked"
+        percent={Math.round((latest.calls_booked / 10) * 100)}
+        caption={`${latest.calls_booked}/10`}
+      />
     </div>
   );
 }
