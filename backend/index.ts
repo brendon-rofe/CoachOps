532c23b1a309c37d26ec588f3863b9979c07ed2c
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-10-21 16:31:09 +0200

    fix: Import prisma client from the correct place

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,5 @@
 import express, { Express, Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './generated/prisma';
 
 const prisma = new PrismaClient();
 
