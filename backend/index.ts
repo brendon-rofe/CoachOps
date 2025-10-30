658fc9cab1cd1b3b156879146ca6157e0ae42b71
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-10-30 15:51:00 +0200

    feat: Create post endpoint for dms

--- backend/index.ts
+++ backend/index.ts
@@ -40,6 +40,22 @@ app.get('/api/connect-requests/:userId', async (req: Request, res: Response) => 
   };
 });
 
+app.post('/api/direct-messages/:userId', async (req: Request, res: Response) => {
+  const userId = Number(req.params.userId);
+  const data = req.body;
+  try {
+    const connectRequest = await prisma.directMessage.create({
+      data: {
+        userId,
+        ...data
+      }
+    });
+    res.json(connectRequest);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 app.get('/api/direct-messages/:userId', async (req: Request, res: Response) => {
   const userId = Number(req.params.userId);
   try {
